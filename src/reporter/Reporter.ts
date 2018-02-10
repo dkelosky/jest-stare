d0b96a6ea276507688dc87cb0d94740d0f8f0934
Dan Kelosky <daniel.kelosky@ca.com>
2018-02-10 16:14:31 -0500

    do doc right

--- src/reporter/Reporter.ts
+++ src/reporter/Reporter.ts
@@ -1,6 +1,18 @@
 import GlobalConfig = jest.GlobalConfig;
 
+/**
+ * Class to implement basic reporter methods
+ * @export
+ * @class Reporter
+ */
 export class Reporter {
+
+    /**
+     * Creates an instance of Reporter.
+     * @param {GlobalConfig} mGlobalConfig - jest global config
+     * @param {*} mOptions - jest options in effect
+     * @memberof Reporter
+     */
     constructor(private mGlobalConfig: GlobalConfig, private mOptions: any) {
     }
 
