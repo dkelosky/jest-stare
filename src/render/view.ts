036605650e187586e67b93d144895442374c6680
Dan Kelosky <daniel.kelosky@ca.com>
2018-02-11 14:40:08 -0500

    browserfy

--- src/render/view.ts
+++ src/render/view.ts
@@ -1,2 +1,19 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import * as $ from "jquery";
+import * as chart from "chart.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("@@@@@TEST")
+    $("#test-suites").text("yoyoy");
+    const doughnut = new chart.Chart($("#test-suites"), {
+        type: "doughnut",
+        data: {
+            labels: ["test", "test2"],
+            datasets: [{
+                backgroundColor: ["#008000", "#008888"],
+                data: ["5", "10"]
+            }]
+        }
+    });
+
+});
