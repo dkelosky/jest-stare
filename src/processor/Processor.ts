0587dd9d5a2836c327d9c2ee27d78f7510bd7d43
dogboydog <chris.l.boehm@gmail.com>
2018-02-13 22:46:51 -0500

    allow the user to specify result directory

--- src/processor/Processor.ts
+++ src/processor/Processor.ts
@@ -4,6 +4,7 @@ import { ISubstitute } from "../reporter/doc/ISubstitute";
 import { IO } from "../utils/IO";
 import * as mustache from "mustache";
 import * as path from "path";
+import { IJestStareConfig } from "./doc/IJestStareConfig";
 
 /**
  * Class to post process jest output and summarize information in an html file
@@ -22,6 +23,7 @@ export class Processor {
     public static resultsProcessor(results: IResultsProcessorInput) {
         const substitute: ISubstitute = {};
 
+        const config = Processor.readPackageJson();
         // TODO()
         // test suites
         substitute.testSuitesPassed = results.numPassedTestSuites;
@@ -38,7 +40,8 @@ export class Processor {
         // full report
         substitute.tests = JSON.stringify(results);
 
-        Processor.generateReport("./", substitute);
+        const resultDirectory = config.resultDir == null ? "./jest-stare" : config.resultDir;
+        Processor.generateReport(resultDirectory, substitute);
 
         return results;
     }
@@ -46,33 +49,34 @@ export class Processor {
     /**
      * Create HTML report
      * @private
-     * @param {string} file - location of report
+     * @param {string} resultDir -  directory to save report
      * @param {ISubstitute} substitute - substitution values for mustache render
      * @memberof Processor
      */
-    private static generateReport(file: string, substitute: ISubstitute) {
-        const baseDir = "jest-stare/";
-        const cssDir = baseDir + "css/";
-        const jsDir = baseDir + "js/";
+    private static generateReport(resultDir: string, substitute: ISubstitute) {
+
+        resultDir += "/"; // extra slash just in case it's ommitted by the user 
+        const cssDir = resultDir + "css/";
+        const jsDir = resultDir + "js/";
 
         const main = "index.html";
 
-        IO.mkdirSync(baseDir);
-        IO.mkdirSync(jsDir);
-        IO.mkdirSync(cssDir);
+        IO.mkdirsSync(resultDir);
+        IO.mkdirsSync(jsDir);
+        IO.mkdirsSync(cssDir);
 
         const html = Processor.obtainWebFile("template.html");
         const rendered = mustache.render(html, substitute);
 
-        IO.writeFile(file + baseDir + main, rendered);
+        IO.writeFile(resultDir + main, rendered);
 
         const mainCss = "jest-stare.css";
         const css = Processor.obtainWebFile(mainCss);
-        IO.writeFile(file + cssDir + mainCss, css);
+        IO.writeFile(cssDir + mainCss, css);
 
         const mainJs = "view.js";
         const js = Processor.obtainJsFile(mainJs);
-        IO.writeFile(file + jsDir + mainJs, js);
+        IO.writeFile(jsDir + mainJs, js);
     }
 
     /**
@@ -93,5 +97,27 @@ export class Processor {
      */
     private static obtainJsFile(name: string): string {
         return IO.readFileSync(path.resolve(__dirname + "/../render/" + name));
+    }
+
+    /**
+     * Read from the user's package.json, if present
+     */
+    private static readPackageJson(): IJestStareConfig {
+        const pkgUp = require("pkg-up");
+        const packageJson = pkgUp.sync();
+        if (packageJson !== null) {
+            const packageJsonContents = require("fs").readFileSync(packageJson).toString();
+            const packageJsonObject = JSON.parse(packageJsonContents);
+            if (packageJsonObject["jest-stare"] == null) {
+                // package json found, but no jest stare config 
+                return {};
+            } else {
+                console.log("Package json config " + require("util").inspect(packageJsonObject["jest-stare"]));
+                return packageJsonObject["jest-stare"];
+            }
+        } else {
+            return {};
+        }
+
     }
 }
