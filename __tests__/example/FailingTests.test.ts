4bff98ebf65dd49dd22964bd0b84a49e81990593
dogboydog <chris.l.boehm@gmail.com>
2018-02-23 07:58:06 -0500

    pass snapshot test for now

--- __tests__/example/FailingTests.test.ts
+++ __tests__/example/FailingTests.test.ts
@@ -1,13 +1,14 @@
 describe("Failing Tests", () => {
 
     it("should fail with a snapshot difference", () => {
+      // change this object to fail the test
         const jsonObject: any = {
-            name: "pointy",
-            breed: "corgi",
+            name: "fluffo",
+            breed: "beagle",
             height: 2,
             color: "brown and white"
         };
-        // Uncomment this line to fail the test
-        //   expect(JSON.stringify(jsonObject,null, 2)).toMatchSnapshot();
+ 
+        expect(JSON.stringify(jsonObject,null, 2)).toMatchSnapshot();
     });
 });
