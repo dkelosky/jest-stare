a74406c32e8953a38ff4e9b0eb9aa4dfc510dd65
Dan Kelosky <daniel.kelosky@ca.com>
2018-02-23 09:09:44 -0500

    remove failing test for publish

--- __tests__/example/FailingTests.test.ts
+++ __tests__/example/FailingTests.test.ts
@@ -1,16 +1,20 @@
 describe("Failing Tests", () => {
 
-    it("should fail with a snapshot difference", () => {
-        const jsonObject: any = {
-            name: "pointy",
-            breed: "corgi",
-            height: 2,
-            color: "brown and white"
-        };
-        expect(JSON.stringify(jsonObject, null, 2)).toMatchSnapshot();
+    // it("should fail with a snapshot difference", () => {
+    //     const jsonObject: any = {
+    //         name: "pointy",
+    //         breed: "corgi",
+    //         height: 2,
+    //         color: "brown and white"
+    //     };
+    //     expect(JSON.stringify(jsonObject, null, 2)).toMatchSnapshot();
+    // });
+
+    it("should be a placeholder", () => {
+        expect(true).toBe(true);
     });
 
-    it("should fail without a snapshot difference", () => {
-        expect(true).toBe(false);
-    });
+    // it("should fail without a snapshot difference", () => {
+    //     expect(true).toBe(false);
+    // });
 });
