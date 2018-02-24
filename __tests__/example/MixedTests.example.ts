a16b869e10e1aee0d52cfb777d67a712a2b44a75
dogboydog <chris.l.boehm@gmail.com>
2018-02-24 08:53:29 -0500

    add a few more example tests and new npm scripts

--- __tests__/example/MixedTests.example.ts
+++ __tests__/example/MixedTests.example.ts
@@ -1,6 +1,6 @@
 describe("Mixed Passing and Failing Tests", () => {
 
-    describe("Snapshot differences", () => {
+    describe("Snapshot tests", () => {
         it("should fail with a snapshot difference", () => {
             const jsonObject: any = {
                 name: "ein",
@@ -10,13 +10,25 @@ describe("Mixed Passing and Failing Tests", () => {
             };
             expect(JSON.stringify(jsonObject, null, 2)).toMatchSnapshot();
         });
+
+        it("should pass a snapshot error comparison", ()=>{
+            expect(()=>{
+                throw new Error("Error stays the same");
+            }).toThrowErrorMatchingSnapshot();
+        });
     });
 
     it("should fail due to an error", () => {
         throw new Error("This is an error failure");
     });
 
+
+
     it("Should fail due to a bad expectation", () => {
         expect(false).toEqual(true);
-    })
+    });
+
+    it("Should pass an expect statement", ()=>{
+        expect(0).toEqual(0);
+    });
 });
