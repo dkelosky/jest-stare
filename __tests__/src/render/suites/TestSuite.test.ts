bcab0bbcdbca31c750a96ce587d29514b34a5006
Dan Kelosky <daniel.kelosky@ca.com>
2018-02-27 10:42:40 -0500

    capture known test cases

--- __tests__/src/render/suites/TestSuite.test.ts
+++ __tests__/src/render/suites/TestSuite.test.ts
@@ -4,6 +4,9 @@ import { TestSuite } from "../../../../src/render/suites/TestSuite";
 import { Test } from "../../../../src/render/tests/Test";
 
 const simplePassingTests = require("../../../data/simplePassingTests.json");
+const simpleFailingTests = require("../../../data/simpleFailingTests.json");
+const twoSideBySideFailDiffTests = require("../../../data/twoSideBySideFailDiffTests.json");
+const largeTests = require("../../../data/largeTests.json");
 
 (Test.create as any) = jest.fn( () => {
     const div = document.createElement("div") as HTMLDivElement;
@@ -13,7 +16,19 @@ const simplePassingTests = require("../../../data/simplePassingTests.json");
 
 describe("TestSuite tests", () => {
 
-    it("should create proper elements for passing json tests", () => {
+    it("should create proper elements for simple passing tests", () => {
         expect(TestSuite.create(simplePassingTests)).toMatchSnapshot();
+    });
+
+    it("should create proper elements for simple failing tests", () => {
+        expect(TestSuite.create(simpleFailingTests)).toMatchSnapshot();
+    });
+
+    it("should create proper elements for two diffs in html", () => {
+        expect(TestSuite.create(twoSideBySideFailDiffTests)).toMatchSnapshot();
+    });
+
+    it("should create proper elements for large test > 500 tests", () => {
+        expect(TestSuite.create(twoSideBySideFailDiffTests)).toMatchSnapshot();
     });
 });
