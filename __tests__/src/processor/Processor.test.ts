edb123f0a07639af2b499812279f886626bbc371
Dan Kelosky <daniel.kelosky@ca.com>
2018-02-27 13:50:39 -0500

    verify no logging occurs

--- __tests__/src/processor/Processor.test.ts
+++ __tests__/src/processor/Processor.test.ts
@@ -3,12 +3,27 @@ jest.mock("../../../src/utils/Logger");
 
 import { IO } from "../../../src/utils/IO";
 import { Processor } from "../../../src/processor/Processor";
-import {inspect} from "util";
+import { inspect } from "util";
 import { Logger } from "../../../src/utils/Logger";
+import { IResultsProcessorInput } from "../../../src/processor/doc/jest/IResultsProcessorInput";
 
-const simplePassingTests = require("../../data/simplePassingTests.json");
+const simplePassingTests: IResultsProcessorInput = require("../../data/simplePassingTests.json");
+
+(Logger as any).writeStdout = jest.fn<string>((msg: string) => {
+    return msg;
+});
+
+(Logger as any).writeStderr = jest.fn<string>((msg: string) => {
+    return msg;
+});
 
 describe("Processor tests", () => {
+
+    it("should accept override config on input and not log when requested not to", () => {
+        const processed = Processor.resultsProcessor(simplePassingTests, {log: false});
+        expect((Logger as any).writeStdout).not.toHaveBeenCalled();
+        expect((Logger as any).writeStderr).not.toHaveBeenCalled();
+    });
 
     it("should create a report form an input object", async () => {
         const processed = Processor.resultsProcessor(simplePassingTests);
--- src/utils/__mocks__/Logger.ts
+++ src/utils/__mocks__/Logger.ts
@@ -1,5 +1,13 @@
 import { isNullOrUndefined } from "util";
 
+const stdout = jest.fn<string>((msg: string) => {
+    return msg;
+});
+
+const stderr = jest.fn<string>((msg: string) => {
+    return msg;
+});
+
 export class Logger {
     private static mLog: Logger;
 
@@ -10,7 +18,15 @@ export class Logger {
         return this.mLog;
     }
 
+    private writeStdout = stdout;
+
+    private writeStderr = stderr;
+
     public debug(message: string, ...args: any[]): string {
-        return message;
+        return this.writeStdout(message);
+    }
+
+    public error(message: string, ...args: any[]): string {
+        return this.writeStderr(message);
     }
 }
