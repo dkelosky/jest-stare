22ae7bf2fa1985883d5914bed4e2cd8f0a32c41d
Dan Kelosky <daniel.kelosky@ca.com>
2018-03-20 21:21:10 -0400

    test private render methods

--- __tests__/src/render/Render.test.ts
+++ __tests__/src/render/Render.test.ts
@@ -2,8 +2,11 @@ import * as $ from "jquery";
 import { Render } from "../../../src/render/Render";
 import { Processor } from "../../../src/processor/Processor";
 import { Constants } from "../../../src/processor/Constants";
+import { IResultsProcessorInput } from "../../../src/processor/doc/jest/IResultsProcessorInput";
+import { IChartData } from "../../../src/render/doc/IChartData";
 
-const simplePassingTests = require("../../data/simplePassingTests.json");
+const simplePassingTests: IResultsProcessorInput = require("../../../data/simplePassingTests.json");
+const simpleFailingTests: IResultsProcessorInput = require("../../../data/simpleFailingTests.json");
 
 describe("Render tests", () => {
     it("should create passing tests", () => {
@@ -30,4 +33,26 @@ describe("Render tests", () => {
         // console.log(newdata)
         expect(true).toBe(true); // MatchSnapshot();
     });
+
+    it("should build chart data with no examples", () => {
+        expect((Render as any).buildChartsData(0, 0)).toMatchSnapshot();
+    });
+
+    it("should build chart data with fail examples", () => {
+        expect((Render as any).buildChartsData(0, 1)).toMatchSnapshot();
+    });
+
+    it("should build chart data with pass examples", () => {
+        expect((Render as any).buildChartsData(1, 0)).toMatchSnapshot();
+    });
+
+    it("should build chart data with pass and fail examples", () => {
+        expect((Render as any).buildChartsData(1, 2)).toMatchSnapshot();
+    });
+
+    it("should add snapshot chart data with pass examples", () => {
+        const chartData: IChartData = (Render as any).buildChartsData(2, 0);
+        expect((Render as any).addSnapshotChartData(simplePassingTests, chartData)).toMatchSnapshot();
+    });
+
 });
