e020bc1d06c3536f84d70b36e22cac4d764eaf0a
dkelosky <dkelosky@gmail.com>
2018-03-22 21:59:32 -0400

    delete empty test

--- __tests__/src/render/charts/Doughnut.test.ts
+++
@@ -1,7 +0,0 @@
-import { Doughnut } from "../../../../src/render/charts/Doughnut";
-describe("Doughnut tests", () => {
-
-    it("should do something", () => {
-        expect(true).toBe(true);
-    });
-});
