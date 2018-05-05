16368a6eb333e06525abf92c410ef93f66eb60f7
dogboydog <chris.l.boehm@gmail.com>
2018-05-04 23:43:21 -0400

    fix  additional results processors error message

--- src/utils/EnvironmentalVariables.ts
+++ src/utils/EnvironmentalVariables.ts
@@ -30,7 +30,7 @@ export class EnvironmentalVariables {
             } catch (e) {
                 Logger.get.error("Could not parse additional results processors value." +
                     "It should be a JSON string of an array of strings, like the following: " +
-                    JSON.stringify(["jest-html-reporter"] + "\n You specified: " + additionalResultsProcessorsValue));
+                    JSON.stringify(["jest-html-reporter"]) + "\n You specified: " + additionalResultsProcessorsValue);
             }
         }
         return {
