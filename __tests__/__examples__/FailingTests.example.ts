bcc1048741ab82034fc946feb22cbd2d9ac1f877
Cainã Brazil <caina.brazil@jumia.com>
2018-10-25 11:14:35 +0100

    Added overflow-auto, which fixes the long text breaking flow in diff view. Removed border-gray, was not being used. Updated test snapshots.

--- __tests__/__examples__/FailingTests.example.ts
+++ __tests__/__examples__/FailingTests.example.ts
@@ -3,7 +3,7 @@ describe("Failing Tests", () => {
     describe("Snapshot differences", () => {
         it("should fail with a small snapshot difference", () => {
             const jsonObject: any = {
-                name: "ein",
+                name: "Amet ad commodo ad aliquip elit excepteur laboris. Aliqua consequat exercitation Lorem incididunt esse nulla eiusmod labore eu magna consequat enim enim dolore. Do in reprehenderit qui sint aliquip quis tempor laborum sint quis.Deserunt sunt veniam magna eiusmod adipisicing incididunt pariatur incididunt elit dolore nostrud sit nostrud. Mollit eu ut ea irure consectetur quis. Ullamco aliqua laboris ipsum et laboris in culpa in ea et do. Velit qui ullamco tempor excepteur aute duis exercitation consectetur deserunt enim.Reprehenderit veniam qui eiusmod ipsum consectetur non. Sint nulla Lorem esse fugiat quis deserunt consequat consequat deserunt sunt qui. Officia commodo irure Lorem dolore qui mollit. Consequat ea tempor occaecat ex irure consequat dolore minim ipsum. Occaecat dolor consequat cupidatat consequat.",
                 breed: "corgi",
                 height: 1,
                 color: "brown and white"
