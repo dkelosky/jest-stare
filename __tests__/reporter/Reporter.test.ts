c41729e3a87f0f5a9fd207436b7230e93d9551bb
Dan Kelosky <daniel.kelosky@ca.com>
2019-02-15 19:51:59 -0500

    run repoter

---
+++ __tests__/reporter/Reporter.test.ts
@@ -0,0 +1,29 @@
+import { Processor } from "../../src/processor/Processor";
+import { Reporter } from "../../src/Reporter/Reporter";
+
+describe("Reporter tests", () => {
+    it("should test the repoter", () => {
+        const mockFn = jest.fn((data) => {
+            return data;
+        });
+        (Processor as any).run = mockFn;
+        const globalConf: jest.GlobalConfig = undefined;
+        const rep = new Reporter(globalConf, {});
+
+        const agg: jest.AggregatedResult = undefined;
+        const opt: jest.ReporterOnStartOptions = undefined;
+        rep.onRunStart(agg, opt);
+
+        const test: jest.Test = undefined;
+        rep.onTestStart(test);
+
+        const testResults: jest.TestResult = undefined;
+        rep.onTestResult(test, testResults, agg);
+
+        const context: Set<jest.Context> = undefined;
+        rep.onRunComplete(context, agg);
+
+        const callCount = 3;
+        expect(mockFn).toHaveBeenCalledTimes(callCount);
+    });
+});
