722eeb4b8d53043514b27c61053e95e6d39e44bc
Dan Kelosky <daniel.kelosky@ca.com>
2019-03-05 10:28:55 -0500

    cli tests

--- __tests__/cli/CLI.test.ts
+++ __tests__/cli/CLI.test.ts
@@ -1,7 +1,24 @@
 import { CLI } from "../../src/cli/CLI";
+import { Processor } from "../../src/processor/Processor";
+import { Logger } from "../../src/utils/Logger";
 
 describe("jest-stare cli tests", () => {
-    it("should", () => {
+    Object.defineProperty(Logger, 'mLog', {
+        get: jest.fn(() => {
+            return {
+                error: (message) => {
+                    return message;
+                }
+            };
+        })
+    });
+
+    (Processor as any).run = jest.fn(() => {
+        // do nothing
+    });
+
+    it("should throw error for no args", () => {
+
         const args = [];
         let error;
         try {
@@ -12,16 +29,16 @@ describe("jest-stare cli tests", () => {
         expect(error).toBeDefined();
     });
 
-    it("should", () => {
-        const args =
-            ["results/jest-stare/jest-results.json"];
-        process.argv = args;
-    });
+    // it("should", () => {
+    //     const args =
+    //         ["results/jest-stare/jest-results.json"];
+    //     process.argv = args;
+    // });
 
-    it("should", () => {
-        const args =
-            ["results/jest-stare/jest-results.json",
-                "C:/Program Files/Git/output"];
-        process.argv = args;
-    });
+    // it("should", () => {
+    //     const args =
+    //         ["results/jest-stare/jest-results.json",
+    //             "C:/Program Files/Git/output"];
+    //     process.argv = args;
+    // });
 });
