a17870c38d42abc980a7c4bd8a9d7355a33faf9c
Dan Kelosky <daniel.kelosky@ca.com>
2019-03-07 07:30:15 -0500

    sep from demo

--- __tests__/__examples__/DemoTests.example.ts
+++ __tests__/__examples__/DemoTests.example.ts
@@ -21,28 +21,4 @@ describe("Demonstrate a jest-stare report", () => {
     it("should add a snapshot", () => {
         expect("randomString").toMatchSnapshot();
     });
-
-    it("should pass with image snapshot", () => {
-        const referenceImage = path.resolve(__dirname, "reference_images", "A.png");
-        const imageAtTest = fs.readFileSync(referenceImage);
-
-        expect(imageAtTest).toMatchImageSnapshot();
-    });
-
-    it("should fail with image snapshot", () => {
-        // Stored snapshot is for A.png, so should fail
-        const referenceImage = path.resolve(__dirname, "reference_images", "B.png");
-        const imageAtTest = fs.readFileSync(referenceImage);
-
-        expect(imageAtTest).toMatchImageSnapshot();
-    });
-
-    //To test multiple fails are handled correctly
-    it("should fail with another image snapshot", () => {
-        // Stored snapshot is for A-edited.png, so should fail
-        const referenceImage = path.resolve(__dirname, "reference_images", "A.png");
-        const imageAtTest = fs.readFileSync(referenceImage);
-
-        expect(imageAtTest).toMatchImageSnapshot();
-    });
 });
