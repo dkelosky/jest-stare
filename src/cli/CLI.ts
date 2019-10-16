0aa099c9f8f5032c61788026a44bb2cbfb449296
Steven Hobson-Campbell <xapphire13@live.com>
2019-10-15 22:27:49 -0700

    Add support for specifying coverage link via CLI

--- src/cli/CLI.ts
+++ src/cli/CLI.ts
@@ -4,6 +4,7 @@ import { Logger } from "../utils/Logger";
 import { Constants } from "../processor/Constants";
 import { IO } from "../utils/IO";
 import { Processor } from "../processor/Processor";
+import * as yargs from "yargs";
 
 /**
  * Handle CLI execution
@@ -11,29 +12,45 @@ import { Processor } from "../processor/Processor";
  * @class CLI
  */
 export class CLI {
-
     /**
      * Read args and invoke processor
      * @static
      * @param {string[]} args - array separated arguments
      * @memberof CLI
      */
-    public static run(args: string[]) {
+    public static run(argv: string[]) {
+        const args = yargs
+            .usage("$0 <testResults> [resultDir]", "jest-stare CLI", y =>
+                y
+                    .positional("testResults", { type: "string" })
+                    .positional("resultDir", { type: "string" })
+            )
+            .options({
+                coverageLink: {
+                    type: "string",
+                    description:
+                        "Link to coverage report for convenient referencing in top left of HTML report"
+                }
+            })
+            .strict()
+            .parse(argv);
 
         let config: IJestStareConfig = {};
 
-        if (isNullOrUndefined(args[0])) {
+        if (isNullOrUndefined(args["testResults"])) {
             Logger.get.error(Constants.NO_CLI_INPUT);
             throw new Error();
         }
 
-        if (!isNullOrUndefined(args[1])) {
-            config.resultDir = args[1];
-        } else {
-            config = undefined;
+        if (!isNullOrUndefined(args["resultDir"])) {
+            config.resultDir = args["resultDir"] as string;
         }
 
-        const results = IO.readFileSync(args[0]);
+        if (!isNullOrUndefined(args.coverageLink)) {
+            config.coverageLink = args.coverageLink;
+        }
+
+        const results = IO.readFileSync(args["testResults"] as string);
         Processor.run(JSON.parse(results), config);
     }
 }
