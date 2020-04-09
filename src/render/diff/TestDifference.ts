b6d938a1f635b5d780f5f1898342465017d32110
Dan Kelosky <dkelosky@gmail.com>
2020-04-09 12:10:35 -0400

    match on old diff format

--- src/render/diff/TestDifference.ts
+++ src/render/diff/TestDifference.ts
@@ -12,7 +12,7 @@ export class TestDifference {
      * If this appears in a failure message, it is the beginning of a snapshot
      * diff.
      */
-    public static DIFF_INDICATOR: RegExp = /- Snapshot\s*(\-\s*[0-9]+)?\n\s*\+ Received\s*(\+\s*[0-9]+)/g;
+    public static DIFF_INDICATOR: RegExp = /- Snapshot\s*(\-\s*[0-9]+)?\n\s*\+ Received\s*(\+\s*[0-9]+)?/g;
 
     public static DIFF_END_INDICATOR: RegExp = /(at .*? \(.*?:[0-9]+:[0-9]+\)\s)/g;
 
